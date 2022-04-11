fae540d2905d0272764ce2cedda5310e61751fad
Viktor Malmedal <viktormalmedal@gmail.com>
2022-04-11 20:28:16 +0200

    init commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/JohnVicke/lunch-me-api
+
+go 1.16
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/lib/pq v1.10.5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+
+func main() {
+  err := godotenv.Load(".env")
+
+  if err != nil {
+    log.Fatalf("Error loading .env file")
+  }
+
+  a := App{}
+  a.Initialize(
+    os.Getenv("APP_DB_USERNAME"),
+    os.Getenv("APP_DB_PASSWORD"),
+    os.Getenv("APP_DB_NAME"),
+  )
+  a.Run(os.Getenv("PORT"))
+}
